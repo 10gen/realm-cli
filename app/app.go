7c8159289af4417a09556fde64098e074cc6c279
Lucas Morales <lucas@lucasem.com>
2017-08-22 18:01:33 -0400

    golint app

--- app/app.go
+++ app/app.go
@@ -1,7 +1,11 @@
+// Package app defined the App type which represents the configuration of a
+// stitch application, and provides means for importing and exporting such a
+// configuration.
 package app
 
 import "encoding/json"
 
+// App contains all data relevant to the configuration of a stitch application.
 type App struct {
 	Group, Name, ID, ClientID string
 
@@ -12,26 +16,36 @@ type App struct {
 	AuthProviders []AuthProvider
 }
 
+// Cluster contains data relevant to the configuration of a MongoDB cluster
+// associated with an App.
 type Cluster struct {
 	Name, URI string
 }
 
+// Service contains data relevant to the configuration of any particular
+// service associated with an App.
 type Service struct {
 	Type, Name string
 	Webhooks   []Webhook
 	Rules      []ServiceRule
 }
 
+// Webhook contains data relevant to the configuration of any particular
+// incoming webhook associated with a Service.
 type Webhook struct {
 	Name, ID, Output string
 	Pipeline         json.RawMessage
 }
 
+// ServiceRule contains data relevant to the configuration of any particular
+// rule associated with a Service.
 type ServiceRule struct {
 	Name, ID string
 	Rule     json.RawMessage
 }
 
+// Pipeline contains data relevant to the configuration of any particular named
+// pipeline associated with an App.
 type Pipeline struct {
 	Name, ID, Output      string
 	Private, SkipRules    bool
@@ -39,16 +53,23 @@ type Pipeline struct {
 	CanEvaluate, Pipeline json.RawMessage
 }
 
+// PipelineParameter contains data relevant to the configuration of any
+// particular argument for a named pipeline.
 type PipelineParameter struct {
 	Name     string
 	Required bool
 }
 
+// Value contains data relevant to the configuration of any particular
+// admin-defined "value" associated an App.
 type Value struct {
 	Name  string
 	Value json.RawMessage
 }
 
+// AuthProvider contains data relevant to the configuration of any
+// particular authentication provider associated with an App.
+// Some of these fields are not applicable to all auth providers.
 type AuthProvider struct {
 	Type, Name, ID                             string
 	Enabled                                    bool
--- app/import_export.go
+++ app/import_export.go
@@ -4,11 +4,15 @@ import (
 	"encoding/json"
 )
 
+// Import unmarshals an exported stitch app into the App representation defined
+// in this package.
 func Import(payload []byte) (a App, err error) {
 	err = json.Unmarshal(payload, &a)
 	return
 }
 
+// Export marshals the App such that it may be imported as a stitch application
+// configuration.
 func (a App) Export() []byte {
 	raw, _ := json.Marshal(a)
 	return raw
