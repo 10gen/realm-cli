93cf826340f40f34937f86187bcf00fedf962b7f
Akaash Kambath <akaash.kambath@Akaashs-MacBook-Pro.local>
2019-06-07 15:21:35 -0400

    STITCH-2990: added diff command
    * modified importApp() method in import.go to take in a dry-run flag
    * diff command uses the modified importApp() method with dry-run flag set to true
    * added extra line in main.go to include diff command

---
+++ commands/diff_test.go
@@ -0,0 +1,115 @@
+package commands
+
+import (
+	"io"
+	"testing"
+
+	"github.com/10gen/stitch-cli/models"
+	"github.com/10gen/stitch-cli/user"
+	u "github.com/10gen/stitch-cli/utils/test"
+	"github.com/mitchellh/cli"
+	gc "github.com/smartystreets/goconvey/convey"
+)
+
+func setUpBasicDiffCommand() (*DiffCommand, *cli.MockUi) {
+	mockUI := cli.NewMockUi()
+	cmd, err := NewDiffCommandFactory(mockUI)()
+	if err != nil {
+		panic(err)
+	}
+
+	diffCommand := cmd.(*DiffCommand)
+	diffCommand.storage = u.NewEmptyStorage()
+	diffCommand.writeToDirectory = func(dest string, r io.Reader, overwrite bool) error {
+		return nil
+	}
+	diffCommand.writeAppConfigToFile = func(dest string, app models.AppInstanceData) error {
+		return nil
+	}
+
+	mockStitchClient := &u.MockStitchClient{
+		DiffFn: func(groupID, appID string, appData []byte, strategy string) ([]string, error) {
+			return []string{"sample-diff-contents"}, nil
+		},
+		FetchAppByClientAppIDFn: func(clientAppID string) (*models.App, error) {
+			return &models.App{
+				GroupID: "group-id",
+				ID:      "app-id",
+			}, nil
+		},
+	}
+	diffCommand.stitchClient = mockStitchClient
+	return diffCommand, mockUI
+}
+
+func TestDiffCommand(t *testing.T) {
+	validArgs := []string{"--app-id=my-app-abcdef"}
+
+	t.Run("should require the user to be logged in", func(t *testing.T) {
+		diffCommand, mockUI := setUpBasicDiffCommand()
+		exitCode := diffCommand.Run(validArgs)
+		u.So(t, exitCode, gc.ShouldEqual, 1)
+
+		u.So(t, mockUI.ErrorWriter.String(), gc.ShouldContainSubstring, user.ErrNotLoggedIn.Error())
+	})
+
+	t.Run("when the user is logged in", func(t *testing.T) {
+		setup := func() (*DiffCommand, *cli.MockUi) {
+			diffCommand, mockUI := setUpBasicDiffCommand()
+
+			diffCommand.user = &user.User{
+				APIKey:      "my-api-key",
+				AccessToken: u.GenerateValidAccessToken(),
+			}
+
+			return diffCommand, mockUI
+		}
+
+		type testCase struct {
+			Description      string
+			Args             []string
+			ExpectedExitCode int
+			ExpectedError    string
+			WorkingDirectory string
+			StitchClient     u.MockStitchClient
+		}
+
+		for _, tc := range []testCase{
+			{
+				Description:      "it fails if given an invalid flagAppPath",
+				Args:             append([]string{"--path=/somewhere/bogus"}, validArgs...),
+				ExpectedExitCode: 1,
+				ExpectedError:    "directory does not exist",
+			},
+			{
+				Description:      "it succeeds if given a valid flagAppPath",
+				Args:             append([]string{"--path=../testdata/full_app"}, validArgs...),
+				ExpectedExitCode: 0,
+				StitchClient: u.MockStitchClient{
+					DiffFn: func(groupID, appID string, appData []byte, strategy string) ([]string, error) {
+						return []string{"sample-diff-contents"}, nil
+					},
+					FetchAppByClientAppIDFn: func(clientAppID string) (*models.App, error) {
+						return &models.App{
+							GroupID: "group-id",
+							ID:      "app-id",
+						}, nil
+					},
+				},
+			},
+		} {
+			t.Run(tc.Description, func(t *testing.T) {
+				diffCommand, mockUI := setup()
+
+				diffCommand.stitchClient = &tc.StitchClient
+				diffCommand.workingDirectory = tc.WorkingDirectory
+
+				exitCode := diffCommand.Run(tc.Args)
+				u.So(t, exitCode, gc.ShouldEqual, tc.ExpectedExitCode)
+				u.So(t, mockUI.ErrorWriter.String(), gc.ShouldContainSubstring, tc.ExpectedError)
+			})
+		}
+
+	})
+
+}
