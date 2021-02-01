5e5e41ae423ea88c814d7fb8d1bb05a61985ad0d
kpatel71716 <kush.patel@mongodb.com>
2021-02-01 15:40:24 -0600

    REALMC-7162 move test app setup to app_test

--- internal/cloud/realm/app_test.go
+++ internal/cloud/realm/app_test.go
@@ -1,6 +1,7 @@
 package realm_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -49,4 +50,17 @@ func TestRealmApps(t *testing.T) {
 			})
 		})
 	})
+}
+
+func setupTestApp(t *testing.T, client realm.Client, groupID, name string) (realm.App, func()) {
+	t.Helper()
+	app, err := client.CreateApp(groupID, name, realm.AppMeta{})
+	assert.Nil(t, err)
+	teardown := func() {
+		deleteErr := client.DeleteApp(groupID, app.ID)
+		if deleteErr != nil {
+			t.Log(fmt.Sprintf("failed to delete test app with groupID: %v and ID: %v", groupID, app.ID))
+		}
+	}
+	return app, teardown
 }
