e5cbe73101c14365acbf513455e84e0436e50eb7
Nick Makes <nick@makes.life>
2021-03-01 14:57:32 -0500

    REALMC-8016: Include data source rules in new import request

--- internal/local/structure_v2_test.go
+++ internal/local/structure_v2_test.go
@@ -35,3 +35,34 @@ func TestParseFunctionsV2(t *testing.T) {
 		}, functions)
 	})
 }
+
+func TestParseDataSources(t *testing.T) {
+	wd, wdErr := os.Getwd()
+	assert.Nil(t, wdErr)
+
+	testRoot := filepath.Join(wd, "testdata/data_sources")
+
+	t.Run("should return the parsed data sources directory with nested rules and schema", func(t *testing.T) {
+		dataSources, err := parseDataSources(testRoot)
+		assert.Nil(t, err)
+		assert.Equal(t, []DataSourceStructure{{
+			Config: map[string]interface{}{
+				"type":   "mongodb-atlas",
+				"name":   "mongodb-atlas",
+				"config": map[string]interface{}{},
+			},
+			Rules: []map[string]interface{}{
+				{
+					"database":   "foo",
+					"collection": "bar",
+					"schema":     map[string]interface{}{"title": "foo.bar schema"},
+				},
+				{
+					"database":   "test",
+					"collection": "test",
+					"schema":     map[string]interface{}{"title": "test.test schema"},
+				},
+			},
+		}}, dataSources)
+	})
+}
