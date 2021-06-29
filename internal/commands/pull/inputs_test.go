379198d2b24daa002ef8df846c096c8efde4e297
Jordan Tse <jordan.tse@mongodb.com>
2021-06-29 13:56:06 -0400

    REALMC-8257 Fix linter issues

--- internal/commands/pull/inputs_test.go
+++ internal/commands/pull/inputs_test.go
@@ -304,12 +304,12 @@ func TestPullTemplatesResolve(t *testing.T) {
 			result, err := input.resolveClient(ui, realmClient, "some-group-id", "some-group-app")
 			assert.Nil(t, err)
 			assert.Equal(t, 2, len(result))
-			expected := map[string]*zip.ReadCloser {
-				"some-template-id": templateZipPkg1,
+			expected := map[string]*zip.ReadCloser{
+				"some-template-id":    templateZipPkg1,
 				"another-template-id": templateZipPkg2,
 			}
-			for templateId, zipPkg := range result {
-				compareZipPackages(t, expected[templateId], zipPkg)
+			for templateID, zipPkg := range result {
+				compareZipPackages(t, expected[templateID], zipPkg)
 			}
 		})
 	})
