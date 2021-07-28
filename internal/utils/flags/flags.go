59f51b426861fb2d9feb35f2cdda5b80f793e0ec
Andrew Chen <andrew.chen@10gen.com>
2021-07-28 11:32:20 -0400

    Update cmd factory to include NoArgsDefaultValue
    This allows us to in order to support creation of flags that have different behavior when passed
    with no args vs. when they have a user-defined argument

--- internal/utils/flags/flags.go
+++ internal/utils/flags/flags.go
@@ -179,8 +179,9 @@ func NewStringSetFlag(values *[]string, opts StringSetOptions) CustomFlag {
 // StringFlag is a string flag
 type StringFlag struct {
 	Meta
-	Value        *string
-	DefaultValue string
+	Value              *string
+	DefaultValue       string
+	NoArgsDefaultValue *string
 }
 
 // Register registers the string flag with the provided flag set
@@ -189,6 +190,13 @@ func (f StringFlag) Register(fs *pflag.FlagSet) {
 		fs.StringVar(f.Value, f.Name, f.DefaultValue, f.Usage.String())
 	} else {
 		fs.StringVarP(f.Value, f.Name, f.Shorthand, f.DefaultValue, f.Usage.String())
+	}
+
+	// If NoArgsDefaultValue is defined, set a default value when flag is defined with no args
+	if f.NoArgsDefaultValue != nil {
+		if flag := fs.Lookup(f.Name); flag != nil {
+			flag.NoOptDefVal = *f.NoArgsDefaultValue
+		}
 	}
 
 	if f.Hidden {
