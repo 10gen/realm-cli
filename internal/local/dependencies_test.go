65bf8424a2335bfc79b7b4572e76c093477e7fa5
Rider Wang <ryderhwang@gmail.com>
2021-09-08 12:57:36 -0500

    update test

--- internal/local/dependencies_test.go
+++ internal/local/dependencies_test.go
@@ -107,7 +107,7 @@ func TestDependenciesFindPackageJSON(t *testing.T) {
 		assert.NotNil(t, err)
 		assert.Equal(t,
 			err.Error(),
-			"stat /Users/rider.wang/Mongo/realm-cli/internal/local/testdata/dependencies/empty/functions/package.json: no such file or directory",
+			fmt.Sprintf("stat %s/functions/package.json: no such file or directory", dir),
 		)
 	})
 
