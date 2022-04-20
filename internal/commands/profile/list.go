b4a3d8a998340b3d18d5afe299e3059c70f7c81f
Jamie Anderson <jamie.anderson@mongodb.com>
2022-04-20 14:51:48 -0400

    add profile list command

---
+++ internal/commands/profile/list.go
@@ -0,0 +1,43 @@
+package profile
+
+import (
+	"fmt"
+	"github.com/10gen/realm-cli/internal/cli"
+	"github.com/10gen/realm-cli/internal/cli/user"
+	"github.com/10gen/realm-cli/internal/terminal"
+	"github.com/10gen/realm-cli/internal/utils/flags"
+)
+
+// CommandMetaList is the command meta for the `profile list` command
+var CommandMetaList = cli.CommandMeta{
+	Use:         "list",
+	Aliases:     []string{"ls"},
+	Display:     "profiles list",
+	Description: "List your profiles",
+	HelpText:    `Lists and filters your profiles.`,
+}
+
+// CommandList is the `profile list` command
+type CommandList struct {
+	inputs cli.ProjectInputs
+}
+
+// Flags is the command flags
+func (cmd *CommandList) Flags() []flags.Flag {
+	return []flags.Flag{
+		cli.AppFlagWithDescription(&cmd.inputs.App, "Filter the list of profiles by name"),
+		cli.ProjectFlag(&cmd.inputs.Project),
+		cli.ProductFlag(&cmd.inputs.Products),
+	}
+}
+
+// Handler is the command handler
+func (cmd *CommandList) Handler(profile *user.Profile, ui terminal.UI, clients cli.Clients) error {
+	profiles, err := user.ProfileNames()
+	if err != nil {
+		return err
+	}
+
+	ui.Print(terminal.NewListLog(fmt.Sprintf("Found %d profiles", len(profiles)), profiles...))
+	return nil
+}
